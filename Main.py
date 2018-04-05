3853debe13324be101bb61e345374848ef9056dd
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2018-04-05 17:03:26 +0300

    grey scale image format

--- Main.py
+++ Main.py
@@ -1,7 +1,33 @@
+
+from PIL import Image
+from numpy import array
+import numpy
 import cv2
+import matplotlib.pyplot as plt
+import pylab as pl
+import matplotlib.cm as cm
+import numpy as np
 
-img = cv2.imread('Octocat.jpg')
-cv2.imshow('Image',img)
-img.shape
 
+#pl.imshow(im, origin='lower')
+#pl.show()
 
+# ayar
+numpy.set_printoptions(threshold=numpy.nan)
+# image 
+img = Image.open('octocat.jpg')
+
+# convert the image to *greyscale*
+img_grey = img.convert('L')       
+
+# convert to numpy array
+img_array = np.array(img_grey)
+
+# black white image kaydet
+Image.fromarray(img_array).save("octocat_bw.png")
+# show image
+pl.imshow(img_array, cmap=cm.Greys_r)
+pl.show() 
+# image info and print array 
+print(img_grey)
+print(np.array(img_grey))
